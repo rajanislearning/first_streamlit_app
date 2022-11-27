e4c30716494e9d5d3982cfacc3fefb94a4ec60f7
Karthik Rajan <119158981+rajanislearning@users.noreply.github.com>
2022-11-28 00:27:03 +0530

    More items added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit
 streamlit.title("My Home")
 streamlit.header("Breakfast Menu")
-streamlit.text("Oatmeal")
+streamlit.text("🥗 Oatmeal")
 streamlit.text("egg") 
 streamlit.text("dosa")
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+

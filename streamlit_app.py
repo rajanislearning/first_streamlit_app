1761764336b1a8fa9aa4afbf86b7feb408214af8
Karthik Rajan <119158981+rajanislearning@users.noreply.github.com>
2022-11-28 00:24:44 +0530

    added new item

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,5 @@ import streamlit
 streamlit.title("My Home")
 streamlit.header("Breakfast Menu")
 streamlit.text("Oatmeal")
-streamlit.text("egg")                
+streamlit.text("egg") 
+streamlit.text("dosa")
